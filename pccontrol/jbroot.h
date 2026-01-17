7ef1974c8d88b594d5e02d1a0a4d8f1c1ba12475
rogi747 <rogijame@gmail.com>
2026-01-17 09:49:08 -0800

    Detect rootless prefix at runtime

--- pccontrol/jbroot.h
+++ pccontrol/jbroot.h
@@ -1,7 +1,33 @@
 #ifndef JBROOT_H
 #define JBROOT_H
 
-#define JBROOT_PATH(path) path
-#define JBROOT_PATH_OC(path) @path
+#import <Foundation/Foundation.h>
+
+#include <limits.h>
+#include <stdio.h>
+#include <unistd.h>
+
+static inline int JBRootIsRootless(void) {
+    return access("/var/jb", F_OK) == 0;
+}
+
+static inline const char *JBRootPathC(const char *path) {
+    if (!JBRootIsRootless()) {
+        return path;
+    }
+    static char buffer[PATH_MAX];
+    snprintf(buffer, sizeof(buffer), "/var/jb%s", path);
+    return buffer;
+}
+
+static inline NSString *JBRootPathOC(NSString *path) {
+    if (!JBRootIsRootless()) {
+        return path;
+    }
+    return [@"/var/jb" stringByAppendingString:path];
+}
+
+#define JBROOT_PATH(path) JBRootPathC(path)
+#define JBROOT_PATH_OC(path) JBRootPathOC(@path)
 
 #endif
